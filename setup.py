e4b4b6af60a81b4b0d8129f3c669da7566556217
Phillip Kuhrt <mail@phi1010.com>
2021-08-28 02:48:35 +0200

    Sources

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='decorated_paho_mqtt',
+    version='1.0.3',
+    url='https://github.com/phi1010/decorated-paho-mqtt',
+    author='Phillip Kuhrt',
+    author_email='mail@phi1010.com',
+    description='Wrapper for Paho MQTT with declarative subscriptions and topic parsing utilities',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    install_requires=[
+        'paho-mqtt',
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
+
---
+++ src/decorated_paho_mqtt/__init__.py
@@ -0,0 +1,7 @@
+from .mqtt_framework import GenericMqttEndpoint
+from .mqtt_framework import pack_topic, unpack_topic
+
+__all__ = [
+    "GenericMqttEndpoint",
+    "pack_topic", "unpack_topic",
+]
